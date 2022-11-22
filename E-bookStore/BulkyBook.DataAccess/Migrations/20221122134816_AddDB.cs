835bfa4d21c5ce170139b08fbce85d6238503198
TeleQuestAI <harpreetsg.delhi@gmail.com>
2022-11-22 19:41:19 +0530

    Added DB

---
+++ E-bookStore/BulkyBook.DataAccess/Migrations/20221122134816_AddDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    public partial class AddDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
